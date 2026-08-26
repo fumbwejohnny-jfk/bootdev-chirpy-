b59c2f15b60170c8cd70fc0614802e0effdace39
Johnny <fumbwejohnny@gmail.com>
2026-08-26 14:49:46 +0200

    routing — middleware

--- main.go
+++ main.go
@@ -1,8 +1,9 @@
 package main
 
 import (
-	// "fmt"
 	"net/http"
+	"fmt"
+	"github.com/fumbwejohnny-jfk/bootdev-chirpy/middleware"
 )
 
 
@@ -10,16 +11,43 @@ func main(){
 
 	router := new(http.ServeMux)
 
+	// get api config
+	cfg := middleware.NewApiConfig()
+	
+
 	// Serve files from the current directory
 	fileServer := http.FileServer(http.Dir("."))
 	
 	// Handle requests to the root path
-	router.Handle("/app/", http.StripPrefix("/app", fileServer))
+	// router.Handle("/app/",middleware.MiiddlewareLog(http.StripPrefix("/app", fileServer)))
+	router.Handle("/app/", cfg.MiddlewareMetricsInc(http.StripPrefix("/app", fileServer)))
 	
+	// Handle the number of requests
+	router.HandleFunc("GET /admin/metrics", func(w http.ResponseWriter, r *http.Request){
+		w.Header().Set("Content-Type", "text/html; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		text := fmt.Sprintf(`
+			<html>	
+				<body>
+					<h1>Welcome, Chirpy Admin</h1>
+					<p>Chirpy has been visited %d times!</p>
+				</body>
+			</html>`, cfg.GetMetrics())
+		w.Write([]byte(text))
+	})	
+
+	// Handle reset
+	router.HandleFunc("POST /admin/reset", func(w http.ResponseWriter, r *http.Request){
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		cfg.ResetMetrics()
+		counter := fmt.Sprintf("Hits: %v", cfg.GetMetrics())
+		w.Write([]byte(counter))
+	})	
 
 	// Readiness endpoint
-	router.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "text/plain")
+	router.HandleFunc("GET /api/healthz", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 		w.WriteHeader(http.StatusOK)
 		w.Write([]byte("OK"))
 	})
@@ -29,9 +57,11 @@ func main(){
 
 	server := http.Server{
 		Handler: router,
-		Addr:    ":8000",
+		Addr:    ":8080",
 	}
 
+	// server stats
+	// mux.Handle("/app/", middleware.MiddlewareLog(handler))
 	server.ListenAndServe()
 
 }
---
+++ middleware/middleware.go
@@ -0,0 +1,39 @@
+package middleware
+
+import (
+	"log"
+	"net/http"
+	"sync/atomic"
+)
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+
+func NewApiConfig() *apiConfig{
+	return &apiConfig{}
+}
+
+
+func MiiddlewareLog(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("%s %s", r.Method, r.URL.Path)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) MiddlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) GetMetrics() int32 {
+	return cfg.fileserverHits.Load()
+}
+
+func (cfg *apiConfig) ResetMetrics(){
+	cfg.fileserverHits.Store(0)
+}
